7eba4f1825175ae5c06df4260d54116fa748222e
Homa3030 <homa30302000@gmail.com>
2022-05-24 01:36:09 +0600

    Add tables

--- src/main/java/Intepreter/TypeChecker.java
+++ src/main/java/Intepreter/TypeChecker.java
@@ -1,10 +1,7 @@
 package Intepreter;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import Intepreter.Utils.Pair;
-import org.junit.Assert;
 import sample.Absyn.*;
 import sample.PrettyPrinter;
 
@@ -118,15 +115,8 @@ public class TypeChecker {
         }
 
         if (expr instanceof Var){
-            var variable = context.variables.stream()
-                    .filter(c -> ((Var) expr).ident_.equals(c.ident))
-                    .findAny()
-                    .orElse(null);
-            if (variable == null)
-                variable = globalContext.variables.stream()
-                        .filter(c -> ((Var) expr).ident_.equals(c.ident))
-                        .findAny()
-                        .orElse(null);
+            var ident = ((Var) expr).ident_;
+            var variable = getVariable(context, globalContext, ident);
 
             if (variable != null)
                 return variable.type;
@@ -136,16 +126,129 @@ public class TypeChecker {
             }
         }
 
+        if (expr instanceof TableDecl){
+            var ident = ((Declaration) ((TableDecl) expr).dec_).ident_;
+            var type = ((Declaration) ((TableDecl) expr).dec_).type_;
+            if (isSameType(type, new TableType()))
+                throw new TypeException(new TableType(), type, expr);
+
+            var identVar_1 = ((TableDecl) expr).ident_1;
+            var identVar_2 = ((TableDecl) expr).ident_2;
+
+            var var_1 = getVariable(context, globalContext, identVar_1);
+            var var_2 = getVariable(context, globalContext, identVar_2);
+
+            if (var_1 == null || var_2 == null){
+                undefinedVar(expr);
+                return null;
+            }
+
+            context.tables.add(new Table(ident, type, var_1, var_2));
+            return type;
+        }
+
+        if (expr instanceof GlTableDecl){
+            var ident = ((GlDeclaration) ((GlTableDecl) expr).gldec_).ident_;
+            var type = ((GlDeclaration) ((GlTableDecl) expr).gldec_).type_;
+            if (isSameType(type, new TableType()))
+                throw new TypeException(new TableType(), type, expr);
+
+            var identVar_1 = ((GlTableDecl) expr).ident_1;
+            var identVar_2 = ((GlTableDecl) expr).ident_2;
+
+            var var_1 = getVariable(context, globalContext, identVar_1);
+            var var_2 = getVariable(context, globalContext, identVar_2);
+
+            if (var_1 == null || var_2 == null){
+                undefinedVar(expr);
+                return null;
+            }
+
+            globalContext.tables.add(new Table(ident, type, var_1, var_2));
+            return type;
+        }
+
+        if (expr instanceof InitTableDecl){
+            var ident = ((Declaration) ((InitTableDecl) expr).dec_1).ident_;
+            var type = ((Declaration) ((InitTableDecl) expr).dec_1).type_;
+            if (isSameType(type, new TableType()))
+                throw new TypeException(new TableType(), type, expr);
+
+            var identVar_1 = ((Declaration) ((InitTableDecl) expr).dec_2).ident_;
+            var typeVar_1 = ((Declaration) ((InitTableDecl) expr).dec_2).type_;
+
+            var identVar_2 = ((Declaration) ((InitTableDecl) expr).dec_3).ident_;
+            var typeVar_2 = ((Declaration) ((InitTableDecl) expr).dec_3).type_;
+
+            context.tables.add(new Table(ident, type, new Variable(identVar_1, typeVar_2),
+                    new Variable(identVar_2, typeVar_2)));
+            return type;
+        }
+
+        if (expr instanceof InitGlTableDecl){
+            var ident = ((GlDeclaration) ((InitGlTableDecl) expr).gldec_).ident_;
+            var type = ((GlDeclaration) ((InitGlTableDecl) expr).gldec_).type_;
+            if (isSameType(type, new TableType()))
+                throw new TypeException(new TableType(), type, expr);
+
+            var identVar_1 = ((Declaration) ((InitGlTableDecl) expr).dec_1).ident_;
+            var typeVar_1 = ((Declaration) ((InitGlTableDecl) expr).dec_1).type_;
+
+            var identVar_2 = ((Declaration) ((InitGlTableDecl) expr).dec_2).ident_;
+            var typeVar_2 = ((Declaration) ((InitGlTableDecl) expr).dec_2).type_;
+
+            globalContext.tables.add(new Table(ident, type, new Variable(identVar_1, typeVar_2),
+                    new Variable(identVar_2, typeVar_2)));
+            return type;
+        }
+
+        if (expr instanceof TableElementCall){
+            var tableIdent = ((TableElementCall) expr).ident_1;
+
+            var table =  getTable(context, globalContext, tableIdent);
+            if (table == null){
+                undefinedVar(expr);
+                return null;
+            }
+
+            var varIdent = ((TableElementCall) expr).ident_2;
+            var varType = getVarType(table, varIdent);
+
+            if (varType != null)
+                return varType;
+
+            undefinedVar(expr);
+            return null;
+        }
+
+        if (expr instanceof TableElementAssignment){
+            var tableIdent = ((TableElementAssignment) expr).ident_1;
+
+            var table =  getTable(context, globalContext, tableIdent);
+            if (table == null){
+                undefinedVar(expr);
+                return null;
+            }
+
+            var varIdent = ((TableElementAssignment) expr).ident_2;
+            var varType = getVarType(table, varIdent);
+
+            if (varType == null){
+                undefinedVar(expr);
+                return null;
+            }
+
+            var tableExpr = ((TableElementAssignment) expr).expr_;
+            var exprType = typeOf(context, globalContext, tableExpr);
+
+            if (!isSameType(varType, exprType))
+                throw new TypeException(varType, exprType, tableExpr);
+            return new VoidType();
+        }
+
         if (expr instanceof Assignment){
-            var variable = context.variables.stream()
-                    .filter(c -> ((Assignment) expr).ident_.equals(c.ident))
-                    .findAny()
-                    .orElse(null);
-            if (variable == null)
-                variable = globalContext.variables.stream()
-                        .filter(c -> ((Assignment) expr).ident_.equals(c.ident))
-                        .findAny()
-                        .orElse(null);
+            var ident = ((Assignment) expr).ident_;
+            var variable = getVariable(context, globalContext, ident);
 
             if (variable != null) {
                 var exprType = typeCheck(context, globalContext, ((Assignment) expr).expr_, variable.type);
@@ -225,6 +328,41 @@ public class TypeChecker {
         }
 
         return null;
+    }
+
+    private Type getVarType(Table table, String varIdent) {
+        if (table.variable_1.ident.equals(varIdent))
+            return table.variable_1.type;
+        if (table.variable_2.ident.equals(varIdent))
+            return table.variable_2.type;
+        return null;
+    }
+
+    private Table getTable(Context context, GlobalContext globalContext, String tableIdent) {
+        var table = context.tables.stream()
+                .filter(c -> tableIdent.equals(c.ident))
+                .findAny()
+                .orElse(null);
+        if (table == null)
+            table = globalContext.tables.stream()
+                    .filter(c -> tableIdent.equals(c.ident))
+                    .findAny()
+                    .orElse(null);
+        return table;
+    }
+
+    private Variable getVariable(Context context, GlobalContext globalContext, String ident) {
+        var variable = context.variables.stream()
+                .filter(c -> ident.equals(c.ident))
+                .findAny()
+                .orElse(null);
+        if (variable == null)
+            variable = globalContext.variables.stream()
+                    .filter(c -> ident.equals(c.ident))
+                    .findAny()
+                    .orElse(null);
+
+        return variable;
     }
 
     private Type checkAndGetProgramType(Context context, GlobalContext globalContext, List<Expr> body, Type expectedType) throws TypeException{
